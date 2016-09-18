1aaff38a0ce328d31dd8139a78f37d28775f37c6
Anton Chekulaev <metaphore@bk.ru>
2016-09-18 14:43:42 +0700

    Version changed to 4.2.0-SNAPSHOT

--- core/src/com/crashinvaders/texturepackergui/AppConstants.java
+++ core/src/com/crashinvaders/texturepackergui/AppConstants.java
@@ -5,5 +5,5 @@ import com.crashinvaders.common.Version;
 public class AppConstants {
     public static final String PREF_NAME_COMMON = "common.xml";
     public static final String PROJECT_FILE_EXT = "tpproj";
-    public static final Version version = new Version("4.1.0");
+    public static final Version version = new Version("4.2.0");
 }
