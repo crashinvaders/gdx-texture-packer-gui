e24b27754327b557cd659b8a63ca2583e27b671f
Anton Chekulaev <metaphore@bk.ru>
2017-04-15 19:08:17 +0700

    Simple AWT window runner

---
+++ desktop/src/com/crashinvaders/texturepackergui/desktop/AwtDesktopLauncher.java
@@ -0,0 +1,89 @@
+package com.crashinvaders.texturepackergui.desktop;
+
+import com.badlogic.gdx.Files;
+import com.badlogic.gdx.backends.lwjgl.LwjglApplicationConfiguration;
+import com.badlogic.gdx.backends.lwjgl.LwjglCanvas;
+import com.crashinvaders.texturepackergui.App;
+import com.crashinvaders.texturepackergui.AppParams;
+import com.github.czyzby.autumn.fcs.scanner.DesktopClassScanner;
+import org.kohsuke.args4j.Argument;
+import org.kohsuke.args4j.CmdLineException;
+import org.kohsuke.args4j.CmdLineParser;
+
+import javax.swing.*;
+import java.awt.*;
+import java.awt.event.WindowAdapter;
+import java.awt.event.WindowEvent;
+import java.io.File;
+
+public class AwtDesktopLauncher {
+	public static void main(final String[] args) {
+		EventQueue.invokeLater(new Runnable() {
+			@Override
+			public void run() {
+				Arguments arguments = new Arguments();
+				try {
+					CmdLineParser parser = new CmdLineParser(arguments);
+					parser.parseArgument(args);
+				} catch (CmdLineException e) {
+					System.out.println("Error: " + e.getLocalizedMessage());
+					return;
+				}
+
+				final LwjglApplicationConfiguration configuration = new LwjglApplicationConfiguration();
+				configuration.title = "LibGDX Texture Packer GUI";
+				configuration.addIcon("icon128.png", Files.FileType.Internal);
+				configuration.addIcon("icon32.png", Files.FileType.Internal);
+				configuration.addIcon("icon16.png", Files.FileType.Internal);
+				configuration.preferencesDirectory = ".gdxtexturepackergui";
+				configuration.width = 1024;
+				configuration.height = 600;
+
+				AppParams appParams = new AppParams();
+				appParams.startupProject = arguments.project;
+
+				App app = new App(new DesktopClassScanner(), appParams);
+//				LwjglCanvas canvas = new LwjglCanvas(new WindowParamsPersistingApplicationWrapper(app, configuration), configuration);
+				LwjglCanvas canvas = new LwjglCanvas(app);
+
+				MainFrame mainFrame = new MainFrame(canvas);
+			}
+		});
+	}
+
+	private static class MainFrame extends JFrame {
+
+		private final LwjglCanvas lwjglCanvas;
+
+		public MainFrame(LwjglCanvas lwjglCanvas) {
+			this.lwjglCanvas = lwjglCanvas;
+
+			addWindowListener(new WindowAdapter() {
+				@Override
+				public void windowClosed(WindowEvent e) {
+					System.exit(0);
+				}
+			});
+
+			initializeComponents();
+		}
+
+		private void initializeComponents() {
+			//FRAME PARAMS
+			{
+				setSize(1024, 768);
+				setLocation(0, 0);
+//				setExtendedState(JFrame.MAXIMIZED_BOTH);
+				setLocationRelativeTo(null);
+				setDefaultCloseOperation(WindowConstants.DISPOSE_ON_CLOSE);
+				setVisible(true);
+				getContentPane().add(lwjglCanvas.getCanvas(), BorderLayout.CENTER);
+			}
+		}
+	}
+
+	private static class Arguments {
+		@Argument
+		File project;
+	}
+}
