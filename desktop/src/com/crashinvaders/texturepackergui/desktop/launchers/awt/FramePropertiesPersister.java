fe6c34eb8afdff2f97f51ece3e1cb1599269b0ab
Anton Chekulaev <metaphore@bk.ru>
2017-04-17 18:57:38 +0700

    AWT frame extended state saves correctly when is not maximized

--- desktop/src/com/crashinvaders/texturepackergui/desktop/launchers/awt/FramePropertiesPersister.java
+++ desktop/src/com/crashinvaders/texturepackergui/desktop/launchers/awt/FramePropertiesPersister.java
@@ -12,17 +12,23 @@ class FramePropertiesPersister {
 
     public static void saveFrameProperties(JFrame frame) {
         int extendedState = frame.getExtendedState();
-        frame.setExtendedState(Frame.NORMAL);
+        boolean extendedStateChanged = false;
+        if (extendedState != Frame.NORMAL) {
+            frame.setExtendedState(Frame.NORMAL);
+            extendedStateChanged = true;
+        }
 
         Preferences prefs = Gdx.app.getPreferences(PREF_NAME);
         prefs.putInteger("x", frame.getX());
         prefs.putInteger("y", frame.getY());
         prefs.putInteger("width", frame.getWidth());
         prefs.putInteger("height", frame.getHeight());
-        prefs.putBoolean("maximized", extendedState == Frame.MAXIMIZED_BOTH);
+        prefs.putInteger("extendedState", extendedState != Frame.ICONIFIED ? extendedState : Frame.NORMAL); // Do not save Frame.ICONIFIED
         prefs.flush();
 
-        frame.setExtendedState(extendedState);
+        if (extendedStateChanged) {
+            frame.setExtendedState(extendedState);
+        }
     }
 
     public static void loadFrameProperties(JFrame frame) {
@@ -36,7 +42,7 @@ class FramePropertiesPersister {
         int height = MathUtils.clamp(prefs.getInteger("height", frame.getSize().height), 320, screenHeight);
         int x = MathUtils.clamp(prefs.getInteger("x", frame.getLocation().x), 0, screenWidth - width);
         int y = MathUtils.clamp(prefs.getInteger("y", frame.getLocation().y), 0, screenHeight - height);
-        int extendedState = prefs.getBoolean("maximized", false) ? Frame.MAXIMIZED_BOTH : frame.getExtendedState();
+        int extendedState = prefs.getInteger("extendedState", frame.getExtendedState());
 
         frame.setSize(width, height);
         frame.setLocation(x, y);
