d933233bc4d1c5cd20ff7f61f8bd59a1ad5ba2a0
Anton Chekulaev <metaphore@bk.ru>
2017-05-25 09:38:32 +0700

    Error notification mechanism for nine patch editor dialog

--- core/src/com/crashinvaders/texturepackergui/controllers/ninepatcheditor/NinePatchEditorDialog.java
+++ core/src/com/crashinvaders/texturepackergui/controllers/ninepatcheditor/NinePatchEditorDialog.java
@@ -5,6 +5,7 @@ import com.badlogic.gdx.files.FileHandle;
 import com.badlogic.gdx.scenes.scene2d.ui.Image;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.Stack;
+import com.crashinvaders.texturepackergui.controllers.ErrorDialogController;
 import com.github.czyzby.autumn.annotation.Inject;
 import com.github.czyzby.autumn.mvc.component.ui.InterfaceService;
 import com.github.czyzby.autumn.mvc.stereotype.ViewDialog;
@@ -12,13 +13,16 @@ import com.github.czyzby.lml.annotation.LmlAction;
 import com.github.czyzby.lml.annotation.LmlActor;
 import com.github.czyzby.lml.annotation.LmlAfter;
 import com.github.czyzby.lml.parser.action.ActionContainer;
+import com.kotcrab.vis.ui.widget.VisDialog;
 
 @ViewDialog("lml/ninepatcheditor/dialogNinePatchEditor.lml")
 public class NinePatchEditorDialog implements ActionContainer {
     private static final String TAG = NinePatchEditorDialog.class.getSimpleName();
 
     @Inject InterfaceService interfaceService;
+    @Inject ErrorDialogController errorDialogController;
 
+    @LmlActor("dialog") VisDialog dialog;
     @LmlActor("canvasStack") Stack canvasStack;
     CompositionHolder compositionHolder;
 
@@ -26,7 +30,7 @@ public class NinePatchEditorDialog implements ActionContainer {
 
     @LmlAfter void initView() {
         if (model == null) {
-            Gdx.app.error(TAG, "Model is not initialized. Have you properly called setImageFile() before showing dialog?");
+            showErrorAndHide(new IllegalStateException("Model is not initialized. Have you properly called setImageFile() before showing dialog?"));
             return;
         }
 
@@ -56,6 +60,26 @@ public class NinePatchEditorDialog implements ActionContainer {
         }
 
         if (imageFile.exists());
-        model = new NinePatchEditorModel(imageFile);
+
+        try {
+            model = new NinePatchEditorModel(imageFile);
+        } catch (Exception e) {
+            showErrorAndHide(e);
+        }
+    }
+
+    private void showErrorAndHide(final Exception exception) {
+        Gdx.app.error(TAG, "", exception);
+        Gdx.app.postRunnable(new Runnable() {
+            @Override
+            public void run() {
+                if (dialog != null) {
+                    dialog.hide();
+                }
+                // Show error dialog
+                errorDialogController.setError(exception);
+                interfaceService.showDialog(errorDialogController.getClass());
+            }
+        });
     }
 }
