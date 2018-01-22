1f300c5b9321169dad27ae7614dc025b01b66c23
Anton Chekulaev <metaphore@bk.ru>
2018-01-22 20:53:21 +0700

    Don't use incremental font generation if CJK is disabled; Proper disposal of FreeTypeFontGenerators.

--- core/src/com/crashinvaders/texturepackergui/controllers/ConfigurationController.java
+++ core/src/com/crashinvaders/texturepackergui/controllers/ConfigurationController.java
@@ -10,6 +10,7 @@ import com.badlogic.gdx.scenes.scene2d.Action;
 import com.badlogic.gdx.scenes.scene2d.actions.Actions;
 import com.badlogic.gdx.scenes.scene2d.ui.Skin;
 import com.badlogic.gdx.scenes.scene2d.ui.TooltipManager;
+import com.badlogic.gdx.utils.Array;
 import com.badlogic.gdx.utils.I18NBundle;
 import com.badlogic.gdx.utils.ObjectMap;
 import com.badlogic.gdx.utils.viewport.ScreenViewport;
@@ -23,7 +24,9 @@ import com.crashinvaders.texturepackergui.controllers.projectserializer.ProjectS
 import com.crashinvaders.texturepackergui.controllers.model.ModelService;
 import com.crashinvaders.texturepackergui.controllers.model.ProjectModel;
 import com.github.czyzby.autumn.annotation.Component;
+import com.github.czyzby.autumn.annotation.Destroy;
 import com.github.czyzby.autumn.annotation.Initiate;
+import com.github.czyzby.autumn.mvc.component.asset.AssetService;
 import com.github.czyzby.autumn.mvc.component.i18n.LocaleService;
 import com.github.czyzby.autumn.mvc.component.ui.InterfaceService;
 import com.github.czyzby.autumn.mvc.component.ui.SkinService;
@@ -63,46 +66,71 @@ public class ConfigurationController {
     @AvailableLocales String[] availableLocales = new String[] { "en", "ru", "de", "zh_TW" };
     @I18nBundle String bundlePath = "i18n/bundle";
 
+    /** Keep track of all initialized FreeType font generators to dispose them later. */
+    private final Array<FreeTypeFontGenerator> ftFontGenerators = new Array<>();
+
     @Initiate(priority = VERY_HIGH_PRIORITY)
-    public void initiateSkin(final SkinService skinService, CjkFontExtensionModule cjkFontModule) {
+    public void initiateSkin(AssetService assetService, final SkinService skinService, CjkFontExtensionModule cjkFontModule) {
         Skin skin = new Skin();
         {
+            // FreeType incremental behavior is an awesome flexible solution, but a VERY memory hungry one.
+            // So we will avoid it if it's not required.
+            boolean incrementalFonts = cjkFontModule.isActivated();
+            Gdx.app.log(TAG, "Incremental fonts are " + (incrementalFonts ? "enabled" : "disabled"));
+
+            String defaultCharacters = FreeTypeFontGenerator.DEFAULT_CHARS;
+            // Russian characters
+            defaultCharacters += "АБВГДЕЁЖЗИЙКЛМНОПРСТУФХЦЧШЩЪЫЬЭЮЯабвгдеёжзийклмнопрстуфхцчшщъыьэюя№";
+
             FreeTypeFontGenerator.setMaxTextureSize(2048);
             FreeTypeFontGenerator fontGenerator = new FreeTypeFontGenerator(Gdx.files.internal("VisOpenSansKerned.ttf"));
             FreeTypeFontGenerator.FreeTypeFontParameter paramsDefault = new FreeTypeFontGenerator.FreeTypeFontParameter();
             paramsDefault.color = new Color(0xffffffe8);
             paramsDefault.size = 15;
-            paramsDefault.incremental = true;
+            paramsDefault.incremental = incrementalFonts;
             paramsDefault.renderCount = 1;
             paramsDefault.gamma = 1.0f;
             paramsDefault.hinting = FreeTypeFontGenerator.Hinting.Full;
+            paramsDefault.characters = defaultCharacters;
             FreeTypeFontGenerator.FreeTypeFontParameter paramsSmall = new FreeTypeFontGenerator.FreeTypeFontParameter();
             paramsSmall.color = new Color(0xffffffe8);
             paramsSmall.size = 12;
-            paramsSmall.incremental = true;
+            paramsSmall.incremental = incrementalFonts;
             paramsSmall.renderCount = 1;
             paramsSmall.gamma = 0.5f;
             paramsSmall.hinting = FreeTypeFontGenerator.Hinting.Full;
+            paramsSmall.characters = defaultCharacters;
             FreeTypeFontGenerator.FreeTypeFontParameter paramsBig = new FreeTypeFontGenerator.FreeTypeFontParameter();
             paramsBig.color = new Color(0xffffffe8);
             paramsBig.size = 22;
-            paramsBig.incremental = true;
+            paramsBig.incremental = incrementalFonts;
             paramsBig.renderCount = 1;
             paramsBig.gamma = 0.75f;
             paramsBig.hinting = FreeTypeFontGenerator.Hinting.Full;
+            paramsBig.characters = defaultCharacters;
 
             BitmapFont fontDefault = fontGenerator.generateFont(paramsDefault);
             BitmapFont fontSmall = fontGenerator.generateFont(paramsSmall);
             BitmapFont fontBig = fontGenerator.generateFont(paramsBig);
 
+            if (incrementalFonts) {
+                ftFontGenerators.add(fontGenerator); // Dispose font generator on application termination
+            } else {
+                fontGenerator.dispose();
+                fontGenerator = null;
+            }
+
             if (cjkFontModule.isActivated()) {
                 Gdx.app.log(TAG, "Skin initialized with CJK font.");
+                FreeTypeFontGenerator cjkFontGenerator = new FreeTypeFontGenerator(cjkFontModule.getFontFile());
+                ftFontGenerators.add(cjkFontGenerator); // Dispose font generator on application termination
+
                 FreeTypeFontGenerator.FreeTypeBitmapFontData ftFontDataDefault = (FreeTypeFontGenerator.FreeTypeBitmapFontData) fontDefault.getData();
-                ftFontDataDefault.addGenerator(new FreeTypeFontGenerator(cjkFontModule.getFontFile()));
+                ftFontDataDefault.addGenerator(cjkFontGenerator);
                 FreeTypeFontGenerator.FreeTypeBitmapFontData ftFontDataSmall = (FreeTypeFontGenerator.FreeTypeBitmapFontData) fontSmall.getData();
-                ftFontDataSmall.addGenerator(new FreeTypeFontGenerator(cjkFontModule.getFontFile()));
+                ftFontDataSmall.addGenerator(cjkFontGenerator);
                 FreeTypeFontGenerator.FreeTypeBitmapFontData ftFontDataBig = (FreeTypeFontGenerator.FreeTypeBitmapFontData) fontBig.getData();
-                ftFontDataBig.addGenerator(new FreeTypeFontGenerator(cjkFontModule.getFontFile()));
+                ftFontDataBig.addGenerator(cjkFontGenerator);
             }
 
             skin.add("default-font", fontDefault, BitmapFont.class);
@@ -123,6 +151,13 @@ public class ConfigurationController {
         for (ObjectMap.Entry<String, Color> entry : skin.getAll(Color.class)) {
             Colors.put(entry.key, entry.value);
         }
+    }
+
+    @Destroy void disposeFreeTypeFontGenerators() {
+        for (int i = 0; i < ftFontGenerators.size; i++) {
+            ftFontGenerators.get(i).dispose();
+        }
+        ftFontGenerators.clear();
     }
 
     @Initiate(priority = HIGH_PRIORITY)
