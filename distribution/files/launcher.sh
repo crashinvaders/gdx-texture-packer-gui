972eba444c6f94b57c6c86fb36709aed9309b20d
Anton Chekulaev <metaphore@bk.ru>
2018-07-05 03:14:56 +0300

    Little cleanup.

--- distribution/files/launcher.sh
+++ distribution/files/launcher.sh
@@ -1,3 +1,3 @@
-export SCRIPTDIR=$(dirname "$0")
+SCRIPTDIR=$(dirname "$0")
 cd $SCRIPTDIR
 java -Xms64m -Xmx1024m -XX:+UseG1GC -XX:MinHeapFreeRatio=15 -XX:MaxHeapFreeRatio=30 -jar ./gdx-texturepacker.jar
