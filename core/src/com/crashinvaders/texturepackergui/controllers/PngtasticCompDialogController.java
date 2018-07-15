7ab7c1bc4f0c2ce93ae052b189017f5fb25d46dd
Anton Chekulaev <metaphore@bk.ru>
2018-07-15 16:12:17 +0300

    Better layout for Pngtastic compression options.

--- core/src/com/crashinvaders/texturepackergui/controllers/PngtasticCompDialogController.java
+++ core/src/com/crashinvaders/texturepackergui/controllers/PngtasticCompDialogController.java
@@ -2,13 +2,13 @@ package com.crashinvaders.texturepackergui.controllers;
 
 import com.badlogic.gdx.Gdx;
 import com.crashinvaders.texturepackergui.controllers.model.ModelService;
-import com.crashinvaders.texturepackergui.controllers.model.PngCompressionType;
 import com.crashinvaders.texturepackergui.controllers.model.ProjectModel;
 import com.crashinvaders.texturepackergui.controllers.model.compression.PngCompressionModel;
 import com.crashinvaders.texturepackergui.controllers.model.compression.PngtasticCompressionModel;
 import com.crashinvaders.texturepackergui.controllers.model.filetype.FileTypeModel;
-import com.crashinvaders.texturepackergui.controllers.model.filetype.KtxFileTypeModel;
 import com.crashinvaders.texturepackergui.controllers.model.filetype.PngFileTypeModel;
+import com.crashinvaders.texturepackergui.views.seekbar.IntSeekBarModel;
+import com.crashinvaders.texturepackergui.views.seekbar.SeekBar;
 import com.github.czyzby.autumn.annotation.Inject;
 import com.github.czyzby.autumn.mvc.stereotype.ViewDialog;
 import com.github.czyzby.lml.annotation.LmlAction;
@@ -16,8 +16,6 @@ import com.github.czyzby.lml.annotation.LmlActor;
 import com.github.czyzby.lml.annotation.LmlAfter;
 import com.github.czyzby.lml.parser.action.ActionContainer;
 import com.kotcrab.vis.ui.widget.VisCheckBox;
-import com.kotcrab.vis.ui.widget.spinner.IntSpinnerModel;
-import com.kotcrab.vis.ui.widget.spinner.Spinner;
 
 @ViewDialog(id = "dialog_comp_pngtastic", value = "lml/compression/dialogPngtastic.lml")
 public class PngtasticCompDialogController implements ActionContainer {
@@ -25,8 +23,8 @@ public class PngtasticCompDialogController implements ActionContainer {
 
     @Inject ModelService modelService;
 
-    @LmlActor("spnLevel") Spinner spnLevel;
-    @LmlActor("chbRemoveGamma") VisCheckBox chbRemoveGamma;
+    @LmlActor SeekBar sbCompLevel;
+    @LmlActor VisCheckBox chbRemoveGamma;
 
     private PngtasticCompressionModel compressionModel;
 
@@ -38,18 +36,18 @@ public class PngtasticCompDialogController implements ActionContainer {
         updateValuesFromModel();
     }
 
-    @LmlAction("onLevelValueChanged") void onLevelValueChanged() {
-        int level = ((IntSpinnerModel) spnLevel.getModel()).getValue();
+    @LmlAction void onCompLevelChanged() {
+        int level = ((IntSeekBarModel) sbCompLevel.getModel()).getValue();
         compressionModel.setLevel(level);
     }
 
-    @LmlAction("onRemoveGammaChanged") void onRemoveGammaChanged() {
+    @LmlAction void onRemoveGammaChanged() {
         boolean removeGamma = chbRemoveGamma.isChecked();
         compressionModel.setRemoveGamma(removeGamma);
     }
 
     private void updateValuesFromModel() {
-        ((IntSpinnerModel) spnLevel.getModel()).setValue(compressionModel.getLevel());
+        ((IntSeekBarModel) sbCompLevel.getModel()).setValue(compressionModel.getLevel());
         chbRemoveGamma.setChecked(compressionModel.isRemoveGamma());
     }
 
