7ecaf2375969b61655bc350e425975ee4aa10aed
Anton Chekulaev <metaphore@bk.ru>
2020-10-17 14:13:19 +1100

    Auto restart JVM with start on first thread parameter on MacOS.

--- desktop/src/com/crashinvaders/texturepackergui/desktop/ApplicationStarter.java
+++ desktop/src/com/crashinvaders/texturepackergui/desktop/ApplicationStarter.java
@@ -4,14 +4,77 @@ import com.badlogic.gdx.Files;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3Application;
 import com.badlogic.gdx.backends.lwjgl3.Lwjgl3ApplicationConfigurationX;
 import com.badlogic.gdx.graphics.glutils.HdpiMode;
+import com.badlogic.gdx.scenes.scene2d.utils.UIUtils;
 import com.crashinvaders.texturepackergui.App;
 import com.crashinvaders.texturepackergui.AppConstants;
 import com.crashinvaders.texturepackergui.AppParams;
 import com.github.czyzby.autumn.fcs.scanner.DesktopClassScanner;
 import org.kohsuke.args4j.*;
+import org.lwjgl.system.macosx.LibC;
+
+import java.io.BufferedReader;
+import java.io.InputStreamReader;
+import java.lang.management.ManagementFactory;
+import java.util.ArrayList;
 
 public class ApplicationStarter {
+
+    /**
+     * A great solution for the dreaded MacOS main thread startup issue.
+     * Borrowed from https://github.com/tommyettinger/gdx-liftoff/blob/430f00f5e9caf3b047a5c51926e0d704e8a2571c/src/main/kotlin/com/github/czyzby/setup/main.kt
+     *
+     * Calling this should be done in an if check; if this returns true, the old program should end because a new JVM will
+     * take over, but if it returns false, the program should continue normally. It is meant to allow MacOS to start with
+     * its required '-XstartOnFirstThread' argument, even if the jar wasn't originally started with it.
+     * Taken from https://github.com/crykn/guacamole/blob/master/gdx-desktop/src/main/java/de/damios/guacamole/gdx/StartOnFirstThreadHelper.java .
+     * Thanks crykn/damios!
+     */
+    private static boolean startNewJvmIfRequired() {
+        if (!UIUtils.isMac) {
+            return false;
+        }
+        long pid = LibC.getpid();
+
+        // check whether -XstartOnFirstThread is enabled
+        if ("1".equals(System.getenv("JAVA_STARTED_ON_FIRST_THREAD_" + pid))) {
+            return false;
+        }
+
+        // check whether the JVM was previously restarted
+        // avoids looping, but most certainly leads to a crash
+        if ("true".equals(System.getProperty("jvmIsRestarted"))) {
+            System.err.println("There was a problem evaluating whether the JVM was started with the -XstartOnFirstThread argument");
+            return false;
+        }
+
+        // Restart the JVM with -XstartOnFirstThread
+        ArrayList<String> jvmArgs = new ArrayList<>();
+        String separator = System.getProperty("file.separator");
+        jvmArgs.add(System.getProperty("java.home") + separator + "bin" + separator + "java");
+        jvmArgs.add("-XstartOnFirstThread");
+        jvmArgs.add("-DjvmIsRestarted=true");
+        jvmArgs.addAll(ManagementFactory.getRuntimeMXBean().getInputArguments());
+        jvmArgs.add("-cp");
+        jvmArgs.add(System.getProperty("java.class.path"));
+        jvmArgs.add(System.getenv("JAVA_MAIN_CLASS_" + pid));
+        try {
+            Process process = new ProcessBuilder(jvmArgs).redirectErrorStream(true).start();
+            BufferedReader processOutput = new BufferedReader(new InputStreamReader(process.getInputStream()));
+            String line;
+            while ((line = processOutput.readLine()) != null) {
+                System.out.println(line);
+            }
+            process.waitFor();
+        } catch (Exception e) {
+            System.err.println("There was a problem restarting the JVM");
+            e.printStackTrace();
+        }
+        return true;
+    }
+
     public static void main(final String[] args) {
+        if(startNewJvmIfRequired()) return;
+
         Arguments arguments = new Arguments();
         try {
             CmdLineParser parser = new CmdLineParser(arguments);
