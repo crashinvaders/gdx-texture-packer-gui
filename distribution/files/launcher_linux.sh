4e66eebfe5698e8c5905dce3caec3d0a724a48ea
Anton Chekulaev <metaphore@bk.ru>
2020-11-03 13:49:19 +1100

    Project file association for Linux packages.

--- distribution/files/launcher_linux.sh
+++ distribution/files/launcher_linux.sh
@@ -1,4 +1,4 @@
 #!/usr/bin/env bash
 SCRIPTDIR=$(dirname "$0")
 cd $SCRIPTDIR
-java -Xms64m -Xmx1024m -XX:+UseG1GC -XX:MinHeapFreeRatio=15 -XX:MaxHeapFreeRatio=30 -jar ./gdx-texturepacker.jar
+java -Xms64m -Xmx1024m -XX:+UseG1GC -XX:MinHeapFreeRatio=15 -XX:MaxHeapFreeRatio=30 -jar ./gdx-texturepacker.jar $1
