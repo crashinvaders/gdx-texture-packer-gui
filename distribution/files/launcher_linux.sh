cc0a815e96adf62e22ae43defa48cc802bb1af89
Anton Chekulaev <metaphore@bk.ru>
2021-10-09 21:58:15 +0700

    Linux package builds updated.

--- distribution/files/launcher_linux.sh
+++ distribution/files/launcher_linux.sh
@@ -1,4 +1,5 @@
 #!/usr/bin/env bash
+# Linux launcher
 SCRIPTDIR=$(dirname "$0")
 cd $SCRIPTDIR
-java -Xms64m -Xmx1024m -XX:+UseG1GC -XX:MinHeapFreeRatio=15 -XX:MaxHeapFreeRatio=30 -jar ./gdx-texturepacker.jar $1
+java -Xms64m -Xmx1024m -XX:+UseG1GC -XX:MinHeapFreeRatio=15 -XX:MaxHeapFreeRatio=30 -jar ./gdx-texturepacker-linux.jar $1
