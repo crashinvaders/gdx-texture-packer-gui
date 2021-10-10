742e9689cf3ce0fd8c6efa82f44b422a507d4560
Anton Chekulaev <metaphore@bk.ru>
2021-10-10 23:29:36 +0700

    Use universal jar name inside linux startup script.

--- distribution/files/launcher_linux.sh
+++ distribution/files/launcher_linux.sh
@@ -2,4 +2,4 @@
 # Linux launcher
 SCRIPTDIR=$(dirname "$0")
 cd $SCRIPTDIR
-java -Xms64m -Xmx1024m -XX:+UseG1GC -XX:MinHeapFreeRatio=15 -XX:MaxHeapFreeRatio=30 -jar ./gdx-texturepacker-linux.jar $1
+java -Xms64m -Xmx1024m -XX:+UseG1GC -XX:MinHeapFreeRatio=15 -XX:MaxHeapFreeRatio=30 -jar ./gdx-texturepacker.jar $1
