33ebc5ea12206beffe9e6d501e81597f2ce8d873
Anton Chekulaev <metaphore@bk.ru>
2023-02-18 10:50:03 +0100

    Use GDX logging instead of system output.

--- desktop/src/com/crashinvaders/texturepackergui/desktop/CliApp.java
+++ desktop/src/com/crashinvaders/texturepackergui/desktop/CliApp.java
@@ -1,6 +1,7 @@
 package com.crashinvaders.texturepackergui.desktop;
 
 import com.badlogic.gdx.ApplicationAdapter;
+import com.badlogic.gdx.Gdx;
 import com.badlogic.gdx.files.FileHandle;
 import com.crashinvaders.texturepackergui.controllers.model.PackModel;
 import com.crashinvaders.texturepackergui.controllers.model.ProjectModel;
@@ -11,7 +12,6 @@ import java.io.File;
 
 // This application adapter is only meant to work with the libGDX headless backend.
 public class CliApp extends ApplicationAdapter {
-
     private static final String TAG = CliApp.class.getSimpleName();
 
     private final Arguments args;
@@ -26,25 +26,25 @@ public class CliApp extends ApplicationAdapter {
 
         ProjectModel projectModel = loadProjectModel(args.project);
 
-        System.out.println("Project loaded \"" + args.project + "\"");
-        System.out.println("Total packs: " + projectModel.getPacks().size);
+        Gdx.app.log(TAG, "Project loaded \"" + args.project + "\"");
+        Gdx.app.log(TAG, "Total packs: " + projectModel.getPacks().size);
         for (PackModel pack : projectModel.getPacks()) {
-            System.out.println("Pack: \"" + pack.getName() + "\"");
+            Gdx.app.log(TAG, "Pack: \"" + pack.getName() + "\"");
         }
     }
 
     private static ProjectModel loadProjectModel(File projectFile) {
         if (projectFile == null) {
-            System.err.println("Path to a project file is not specified.");
+            Gdx.app.error(TAG, "Path to a project file is not specified.");
             System.exit(1);
         }
         FileHandle fileHandle = FileUtils.toFileHandle(projectFile);
         if (fileHandle.isDirectory()) {
-            System.err.println("Project file: " + fileHandle + " is a directory and cannot be loaded.");
+            Gdx.app.error(TAG, "Project file: " + fileHandle + " is a directory and cannot be loaded.");
             System.exit(1);
         }
         if (!fileHandle.exists()) {
-            System.err.println("Project file: " + fileHandle + " doesn't exists.");
+            Gdx.app.error(TAG, "Project file: " + fileHandle + " doesn't exists.");
             System.exit(1);
         }
 
