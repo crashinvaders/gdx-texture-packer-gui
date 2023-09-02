4d3eb4a365100d864417e95e3fa07ef6cc95c17b
Anton Chekulaev <metaphore@bk.ru>
2023-09-02 19:46:41 +0200

    NFD code migrated to 3.3.2 API.

--- desktop/src/com/crashinvaders/texturepackergui/desktop/LwjglFileDialogService.java
+++ desktop/src/com/crashinvaders/texturepackergui/desktop/LwjglFileDialogService.java
@@ -10,8 +10,10 @@ import com.crashinvaders.texturepackergui.controllers.FileDialogService;
 import com.github.czyzby.autumn.annotation.Inject;
 import com.github.czyzby.kiwi.util.common.Strings;
 import org.lwjgl.PointerBuffer;
+import org.lwjgl.system.MemoryStack;
 import org.lwjgl.system.MemoryUtil;
-import org.lwjgl.util.nfd.NFDPathSet;
+import org.lwjgl.util.nfd.NFDFilterItem;
+import org.lwjgl.util.nfd.NFDPathSetEnum;
 import org.lwjgl.util.nfd.NativeFileDialog;
 
 import static org.lwjgl.system.MemoryUtil.memFree;
@@ -30,7 +32,7 @@ public class LwjglFileDialogService implements FileDialogService {
         PointerBuffer pathPointer = MemoryUtil.memAllocPointer(1);
 
         try {
-            int status = NativeFileDialog.NFD_PickFolder(initialPath, pathPointer);
+            int status = NativeFileDialog.NFD_PickFolder(pathPointer, initialPath);
 
             if (status == NativeFileDialog.NFD_CANCEL) {
                 callback.canceled();
@@ -44,7 +46,7 @@ public class LwjglFileDialogService implements FileDialogService {
             }
 
             String selectedFolderPath = pathPointer.getStringUTF8(0);
-            NativeFileDialog.nNFD_Free(pathPointer.get(0));
+            NativeFileDialog.NFD_FreePath(pathPointer.get(0));
 
             Array<FileHandle> array = new Array<>();
             array.add(Gdx.files.absolute(selectedFolderPath));
@@ -59,13 +61,13 @@ public class LwjglFileDialogService implements FileDialogService {
 
     @Override
     public void openFile(@Null String dialogTitle, @Null FileHandle initialFile, @Null FileFilter[] fileFilters, Callback callback) {
-        @Null String initialPath = prepareInitialPath(initialFile);
-        @Null String filterList = prepareFilterList(fileFilters);
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            @Null String initialPath = prepareInitialPath(initialFile);
+            @Null NFDFilterItem.Buffer filterList = prepareFilterList(stack, fileFilters);
 
-        PointerBuffer pathPointer = MemoryUtil.memAllocPointer(1);
+            PointerBuffer pp = stack.mallocPointer(1);
 
-        try {
-            int status = NativeFileDialog.NFD_OpenDialog(filterList, initialPath, pathPointer);
+            int status = NativeFileDialog.NFD_OpenDialog(pp, filterList, initialPath);
 
             if (status == NativeFileDialog.NFD_CANCEL) {
                 callback.canceled();
@@ -78,8 +80,8 @@ public class LwjglFileDialogService implements FileDialogService {
                 throw new RuntimeException("Native file dialog error: " + errorText);
             }
 
-            String selectedFilePath = pathPointer.getStringUTF8(0);
-            NativeFileDialog.nNFD_Free(pathPointer.get(0));
+            String selectedFilePath = pp.getStringUTF8(0);
+//                NativeFileDialog.NFD_FreePath(pathPointer.get(0));
 
             Array<FileHandle> array = new Array<>();
             array.add(Gdx.files.absolute(selectedFilePath));
@@ -87,20 +89,18 @@ public class LwjglFileDialogService implements FileDialogService {
             callback.selected(array);
         } catch (Throwable e) {
             defaultFileService.openFile(dialogTitle, initialFile, fileFilters, callback);
-        } finally {
-            memFree(pathPointer);
         }
     }
 
     @Override
     public void openMultipleFiles(@Null String dialogTitle, @Null FileHandle initialFile, @Null FileFilter[] fileFilters, Callback callback) {
-        @Null String initialPath = prepareInitialPath(initialFile);
-        @Null String filterList = prepareFilterList(fileFilters);
+        try (MemoryStack stack = MemoryStack.stackPush()) {
+            @Null String initialPath = prepareInitialPath(initialFile);
+            @Null NFDFilterItem.Buffer filterList = prepareFilterList(stack, fileFilters);
 
-        NFDPathSet pathSet = NFDPathSet.create();
+            PointerBuffer pp = stack.mallocPointer(1);
 
-        try {
-            int status = NativeFileDialog.NFD_OpenDialogMultiple(filterList, initialPath, pathSet);
+            int status = NativeFileDialog.NFD_OpenDialogMultiple(pp, filterList, initialPath);
 
             if (status == NativeFileDialog.NFD_CANCEL) {
                 callback.canceled();
@@ -113,36 +113,55 @@ public class LwjglFileDialogService implements FileDialogService {
                 throw new RuntimeException("Native file dialog error: " + errorText);
             }
 
-            long selectedAmount = NativeFileDialog.NFD_PathSet_GetCount(pathSet);
-            // If nothing is selected, consider the selection operation is canceled.
-            if (selectedAmount == 0) {
-                callback.canceled();
-                return;
-            }
+//            long selectedAmount = pathAmountPointer.get(0);
+//            // If nothing is selected, consider the selection operation is canceled.
+//            if (selectedAmount == 0) {
+//                callback.canceled();
+//                return;
+//            }
+
+//            Array<FileHandle> array = new Array<>();
+//            for (long i = 0; i < selectedAmount; i++) {
+//                String selectedPath = NativeFileDialog.NFD_PathSet_GetPath(pathSet, i);
+//                array.add(Gdx.files.absolute(selectedPath));
+//            }
+
+            long pathSet = pp.get(0);
+            NFDPathSetEnum psEnum = NFDPathSetEnum.calloc(stack);
+            NativeFileDialog.NFD_PathSet_GetEnum(pathSet, psEnum);
 
             Array<FileHandle> array = new Array<>();
-            for (long i = 0; i < selectedAmount; i++) {
-                String selectedPath = NativeFileDialog.NFD_PathSet_GetPath(pathSet, i);
-                array.add(Gdx.files.absolute(selectedPath));
+            int i = 0;
+            while (NativeFileDialog.NFD_PathSet_EnumNext(psEnum, pp) == NativeFileDialog.NFD_OKAY &&
+                    pp.get(0) != MemoryUtil.NULL) {
+                array.add(Gdx.files.absolute(pp.getStringUTF8(0)));
+                NativeFileDialog.NFD_PathSet_FreePath(pp.get(0));
+            }
+
+            NativeFileDialog.NFD_PathSet_FreeEnum(psEnum);
+            NativeFileDialog.NFD_PathSet_Free(pathSet);
+
+            // If nothing is selected, consider the selection operation is canceled.
+            if (array.size == 0) {
+                callback.canceled();
+                return;
             }
 
             callback.selected(array);
         } catch (Throwable e) {
             defaultFileService.openMultipleFiles(dialogTitle, initialFile, fileFilters, callback);
-        } finally {
-            NativeFileDialog.NFD_PathSet_Free(pathSet);
         }
     }
 
     @Override
     public void saveFile(@Null String dialogTitle, @Null FileHandle initialFile, @Null FileFilter[] fileFilters, Callback callback) {
+        try (MemoryStack stack = MemoryStack.stackPush()) {
         @Null String initialPath = prepareInitialPath(initialFile);
-        @Null String filterList = prepareFilterList(fileFilters);
+        @Null NFDFilterItem.Buffer filterList = prepareFilterList(stack, fileFilters);
 
-        PointerBuffer pathPointer = MemoryUtil.memAllocPointer(1);
+        PointerBuffer pp = MemoryUtil.memAllocPointer(1);
 
-        try {
-            int status = NativeFileDialog.NFD_SaveDialog(filterList, initialPath, pathPointer);
+            int status = NativeFileDialog.NFD_SaveDialog(pp, filterList, initialPath, null);
 
             if (status == NativeFileDialog.NFD_CANCEL) {
                 callback.canceled();
@@ -155,8 +174,7 @@ public class LwjglFileDialogService implements FileDialogService {
                 throw new RuntimeException("Native file dialog error: " + errorText);
             }
 
-            String selectedFilePath = pathPointer.getStringUTF8(0);
-            NativeFileDialog.nNFD_Free(pathPointer.get(0));
+            String selectedFilePath = pp.getStringUTF8(0);
 
             Array<FileHandle> array = new Array<>();
             array.add(Gdx.files.absolute(selectedFilePath));
@@ -164,8 +182,6 @@ public class LwjglFileDialogService implements FileDialogService {
             callback.selected(array);
         } catch (Throwable e) {
             defaultFileService.saveFile(dialogTitle, initialFile, fileFilters, callback);
-        } finally {
-            memFree(pathPointer);
         }
     }
 
@@ -176,22 +192,38 @@ public class LwjglFileDialogService implements FileDialogService {
         return fileHandle.file().getAbsolutePath();
     }
 
-    private static @Null String prepareFilterList(@Null FileFilter[] filters) {
+//    private static @Null String prepareFilterList(@Null FileFilter[] filters) {
+//        if (filters == null)
+//            return null;
+//
+//        StringBuilder sb = new StringBuilder();
+//        for (int i = 0; i < filters.length; i++) {
+//            FileFilter filter = filters[i];
+//            if (sb.length > 0) {
+//                sb.append(";");
+//            }
+//            sb.append(Strings.join(",", (Object[]) filter.extensions));
+//        }
+//
+//        if (sb.length == 0)
+//            return null;
+//
+//        return sb.toString();
+//    }
+
+    private static @Null NFDFilterItem.Buffer prepareFilterList(MemoryStack stack, @Null FileFilter[] filters) {
         if (filters == null)
             return null;
 
-        StringBuilder sb = new StringBuilder();
+        NFDFilterItem.Buffer filterBuffer = NFDFilterItem.malloc(filters.length, stack);;
+
         for (int i = 0; i < filters.length; i++) {
             FileFilter filter = filters[i];
-            if (sb.length > 0) {
-                sb.append(";");
-            }
-            sb.append(Strings.join(",", (Object[]) filter.extensions));
+            filterBuffer.get(i)
+                    .name(stack.UTF8(filter.description))
+                    .spec(stack.UTF8(Strings.join(",", (Object[]) filter.extensions)));
         }
 
-        if (sb.length == 0)
-            return null;
-
-        return sb.toString();
+        return filterBuffer;
     }
 }
